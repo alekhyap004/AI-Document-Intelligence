206c094811e53530e0485586cf8f91300b94708b
Alekhya Palepu <alekhyapalepu@gmail.com>
2026-03-08 15:50:43 -0700

    debug test

--- src/backend/main.py
+++ src/backend/main.py
@@ -19,11 +19,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://ai-document-intelligence-murex.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=['*'],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
