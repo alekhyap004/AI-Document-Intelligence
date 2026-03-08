a862d9d41138bf68aea248c441c88470b83e0141
Alekhya Palepu <alekhyapalepu@gmail.com>
2026-03-08 11:24:22 -0700

    remove torch to reduce deployment image size

--- src/backend/ingest.py
+++ src/backend/ingest.py
@@ -2,10 +2,10 @@ import os
 import shutil
 import uuid
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
-
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
 Settings.llm = Ollama(model="llama3.2")
 
 UPLOAD_DIR = "uploads"
